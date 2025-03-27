8e8b859d30fa90ed1f670a4bcf2c2b3c98b7a762
U190709013 <alperbuber800@gmail.com>
2025-03-27 21:46:00 +0300

    hotel-project-devam

---
+++ WebDevelopment/otel-project/js/reservation.js
@@ -0,0 +1,21 @@
+function Reservation(
+  checkIn,
+  checkOut,
+  nOfAdult,
+  nOfChild,
+  name,
+  surname,
+  tckn,
+  email,
+  phone
+) {
+  this.checkIn = checkIn;
+  this.checkOut = checkOut;
+  this.nOfAdult = nOfAdult;
+  this.nOfChild = nOfChild;
+  this.name = name;
+  this.surname = surname;
+  this.tckn = tckn;
+  this.email = email;
+  this.phone = phone;
+}
